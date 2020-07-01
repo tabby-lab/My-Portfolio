f0ac3e9011f6732e101755d2e8bd3eaf1adc2786
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-01 11:55:40 -0500

    fixed contact route in app.ja

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import Main from './components/main';
 import Navbar from './components/navbar';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AboutMe from './components/aboutme';
 import Contact from './components/contact';
+
 
 
 function App() {
@@ -14,8 +15,10 @@ function App() {
 
     <Router>
  <Navbar />
+ <Switch>
     <Route exact path={["/","/aboutme"]} component={AboutMe} />
     <Route exact path={["/","/contact"]} component={Contact} />
+    </Switch>
 </Router>
 
     
