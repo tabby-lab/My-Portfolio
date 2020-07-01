ad40d343d6080759ddeeae90a0e0bab83d29540c
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-01 12:49:34 -0500

    trying to style contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,16 +5,20 @@ import {Form, FormGroup, Label, Input, Button } from 'reactstrap';
 class Contact extends Component {
 render(){
     return (
+    
         <Fragment>
-      <Form>
-        <FormGroup>
+          <div className="center-form">
+      <Form className="form">
+        <FormGroup> 
           <Label for="exampleEmail">Email</Label>
           <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
           <Input type="textarea" name="text" id="exampleText" />
           <Button>Send</Button>
         </FormGroup>
         </Form>
+        </div>
         </Fragment>
+       
 
     )
 }
