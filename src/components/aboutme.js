6abf336ecbf6dacadc1d665f95c1a512f8fa85ef
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-24 23:33:46 -0500

    fixed the main container to be center of page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class About extends Component {
     render() {
         return (
-            <div className="container">
+            <div className="container main">
 
 
                 <div className="container">
