57825261849ba1a16a7e4af04213bac029100d5c
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-27 13:44:01 -0500

    trying to fix buttons on projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,32 +1,66 @@
-import React, { Fragment }from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import '../App.css';
 
 
 
 
 const Projects = () => {
-  
- 
 
- return (
-   <Fragment>
-   <h1>Projects</h1>
-      
-   <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+
+
+  return (
+    
+      <div className="container">
+        <div className="row">
+          <div className="col-sm project">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="../assets1/project1.png" />
+              <Card.Body>
+                <Card.Title>Jobless</Card.Title>
+                <Card.Text>
+                Full-stack application that helps users search for remote jobs in these hard times.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+    <Button>
+    <a href="https://jobless2020.herokuapp.com/ " className="button">Click Here</a>
+    </Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-sm project">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="../assets1/project2.png" />
+              <Card.Body>
+                <Card.Title>Contact Keeper</Card.Title>
+                <Card.Text>
+                MERN application that lets users register and login to their contact keeper to add, delete, and update contacts.
+    </Card.Text>
+    <Button>
+    <a href="https://protected-coast-02689.herokuapp.com/" className="button">Click Here</a>
+    </Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-sm project">
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="../assets1/project3.png" />
+              <Card.Body>
+                <Card.Title>Github Finder</Card.Title>
+                <Card.Text>
+                Full-stack application that helps users search github profiles.
+    </Card.Text>
+    <Button>
+    <a href="https://quirky-mcnulty-d7b8eb.netlify.app/
+ " className="button">Click Here</a>
+    </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>
 
-      </Fragment>
-      
-    );
+   
+  );
 };
-export default Projects
+  export default Projects
