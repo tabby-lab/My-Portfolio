10d450a3c0388e926af24b5c6b7826659039dcfe
Tabby Garcia <tabby.garcia1@gmail.com>
2020-09-23 15:31:10 -0500

    added technologies back, still need to clean up very old code

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -26,29 +26,10 @@ class About extends Component {
                     <p className="bio"><h6>Full Stack Web Developer with a background in Science and certificate holder in Full Stack Web Development from the University of Texas. Passionate about creating visually enriched web applications and delivering user-centric experiences. Collaborative team player recognized for complex problem solving abilities and strong work ethic. With a background in extensive science research, personable background in customer service, and passion for web development, collaborating with others to create meaningful web applications would be a vision fulfilled.</h6>
 
                     </p>
-
-                </div>
-
-
-
-
-
-
-
-
-
-
-
-               
-
-             
-
-                
-                        <div className="container-fluid tech">
-                       
-                            <div className="row">
-
-                            <i className="fab fa-html5 tech"></i>
+               </div>
+               <div className="container-fluid tech">
+               <div className="row">
+                    <i className="fab fa-html5 tech"></i>
                        
 
                        <i className="fab fa-bootstrap tech"></i>
@@ -74,6 +55,43 @@ class About extends Component {
 
                      
                             <i className="fab fa-js tech"></i>
+                            </div>
+
+
+
+                    </div>
+                    
+
+            
+
+
+
+
+
+
+
+
+
+
+
+               
+
+             
+
+                
+                        <div className="container-fluid tech">
+                       
+                            <div className="row">
+
+                            <i className="fab fa-html5 tech"></i>
+                       
+
+                            
+
+                     
+
+                      
+                        
 
 
 
